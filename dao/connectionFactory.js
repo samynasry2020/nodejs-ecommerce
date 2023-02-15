15ecc702b8ba446e3dd9d59e4050900d689106de
samynasry2020 <87795536+samynasry2020@users.noreply.github.com>
2023-02-15 12:04:19 -0800

    Update connectionFactory.js

--- dao/connectionFactory.js
+++ dao/connectionFactory.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 function connectionFactory() {
   return mysql.createConnection({
-    host: process.env.DB_HOST || 'dpg-cfmjf8ha6gdmb5q8dtvg-a',
+    host: process.env.DB_HOST || 'postgres://shop:6gcVHF8ZCwDAf61Mme2GGENsRmt62mFN@dpg-cfmjf8ha6gdmb5q8dtvg-a/shop_33fc',
     user: process.env.DB_USER || 'shop',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || '6gcVHF8ZCwDAf61Mme2GGENsRmt62mFN',
     database: process.env.DB_NAME || 'shop_33fc',
   });
 }
