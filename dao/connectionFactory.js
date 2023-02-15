d642f547dfd6a75c0b1e1980588f9af6f165e18f
samynasry2020 <87795536+samynasry2020@users.noreply.github.com>
2023-02-15 11:57:34 -0800

    Update connectionFactory.js

--- dao/connectionFactory.js
+++ dao/connectionFactory.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 function connectionFactory() {
   return mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
+    host: process.env.DB_HOST || 'dpg-cfmjf8ha6gdmb5q8dtvg-a',
+    user: process.env.DB_USER || 'shop',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'equantom',
+    database: process.env.DB_NAME || 'shop_33fc',
   });
 }
 
